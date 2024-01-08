b41df12cdb77bee9497115827da50979318d0d93
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2024-01-08 19:06:45 +0200

    TEST

---
+++ ClubMembershipApplication/Data/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    internal class Test
+    {
+        // This is a test to check if the commited files are updated in GitHub. Let's check it out!
+    }
+}
